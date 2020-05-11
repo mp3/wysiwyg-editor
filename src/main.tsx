3ff22261de850d08d976299581e336fe6ccceb03
mp3 <im.in.a.coma.xxx@gmail.com>
2020-05-11 19:54:26 +0900

    add main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Main = () => {
+  return (
+    <div>WYSIWIG Editor</div>
+  )
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'))
