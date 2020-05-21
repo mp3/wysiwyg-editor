83d14a634863849b76d453338d7cd3dc459c5da7
mp3 <im.in.a.coma.xxx@gmail.com>
2020-05-21 12:25:28 +0900

    use state on Tooltip

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,5 +1,5 @@
 import { h } from 'preact'
-import { useEffect, useRef } from 'preact/hooks'
+import { useState, useEffect, useRef } from 'preact/hooks'
 import styled from 'styled-components'
 import Quill, { Block } from '../lib/Quill'
 
@@ -10,6 +10,9 @@ type Props = {
 export const Tooltip = (props: Props) => {
   const { quill } = props
   const tooltipControls = useRef<HTMLDivElement>(null)
+  const [isOpen, setIsOpen] = useState(false)
+  const [top, setTop] = useState(0)
+  const [left, setLeft] = useState(0)
 
   useEffect(() => {
     quill.addContainer(tooltipControls.current)
@@ -24,24 +27,29 @@ export const Tooltip = (props: Props) => {
       }
 
       if ((range as any).length === 0) {
-        tooltipControls.current.setAttribute('style', 'display: none;')
+        setIsOpen(false)
         const [block, _offset] = (quill.scroll as any).descendant(Block, (range as any).index)
-        if (block === null && block.domNode.firstChild instanceof HTMLBRElement !== false) {
-          tooltipControls.current.style.display = 'none'
+        if (block === null) {
+          setIsOpen(false)
         }
       } else {
-        tooltipControls.current.style.display = 'none'
+        setIsOpen(false)
 
         const rangeBounds = quill.getBounds(range as any)
-        tooltipControls.current.style.display = 'block'
-        tooltipControls.current.style.top = `${rangeBounds.bottom + 10}px`
-        tooltipControls.current.style.left = `${rangeBounds.left + rangeBounds.width/2 - tooltipControls.current.offsetWidth/2}px`
+        setIsOpen(true)
+        setTop(rangeBounds.bottom + 10)
+        setLeft(rangeBounds.left + rangeBounds.width/2 - tooltipControls.current.offsetWidth/2)
       }
     })
   }, [])
 
   return (
-    <TooltipControls ref={tooltipControls}>
+    <TooltipControls
+      ref={tooltipControls}
+      isOpen={isOpen}
+      top={top}
+      left={left}
+    >
       <Button onClick={() => quill.format('bold', true)}>
         <i className="fa fa-bold"></i>
       </Button>
@@ -68,12 +76,20 @@ export const Tooltip = (props: Props) => {
   )
 }
 
-const TooltipControls = styled.div`
+type TooltipControlsProps = {
+  isOpen: boolean
+  top: number
+  left: number
+}
+
+const TooltipControls = styled.div<TooltipControlsProps>`
+  visibility: ${props => props.isOpen ? 'visible' : 'hidden'};
   background-color: #111;
   border-radius: 4px;
-  display: none;
   padding: 5px 10px;
   position: absolute;
+  top: ${props => `${props.top}px`};
+  left: ${props => `${props.left}px`};
 
   &::before {
     box-sizing: border-box;
