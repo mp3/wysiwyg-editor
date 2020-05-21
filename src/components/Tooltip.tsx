ecb476acfb9b23a00daf92491c464f8f8ad38b70
mp3 <im.in.a.coma.xxx@gmail.com>
2020-05-21 09:48:10 +0900

    fix

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -25,9 +25,7 @@ export const Tooltip = (props: Props) => {
       if ((range as any).length === 0) {
         tooltipControls.current.setAttribute('style', 'display: none;')
         const [block, _offset] = (quill.scroll as any).descendant(Block, (range as any).index)
-        if (block !== null && block.domNode.firstChild instanceof HTMLBRElement) {
-
-        } else {
+        if (block === null && block.domNode.firstChild instanceof HTMLBRElement !== false) {
           tooltipControls.current.style.display = 'none'
         }
       } else {
@@ -43,12 +41,12 @@ export const Tooltip = (props: Props) => {
 
   return (
     <div id="tooltip-controls" ref={tooltipControls}>
-      <button id="bold-button" onClick={() => quill?.format('bold', true)}><i className="fa fa-bold"></i></button>
-      <button id="italic-button" onClick={() => quill?.format('italic', true)}><i className="fa fa-italic"></i></button>
-      <button id="link-button" onClick={() => quill?.format('link', prompt('Enter link URL'))}><i className="fa fa-link"></i></button>
-      <button id="blockquote-button" onClick={() => quill?.format('blockquote', true)}><i className="fa fa-quote-right"></i></button>
-      <button id="header-1-button" onClick={() => quill?.format('header', 1)}><i className="fa fa-header"><sub>1</sub></i></button>
-      <button id="header-2-button" onClick={() => quill?.format('header', 2)}><i className="fa fa-header"><sub>2</sub></i></button>
+      <button id="bold-button" onClick={() => quill.format('bold', true)}><i className="fa fa-bold"></i></button>
+      <button id="italic-button" onClick={() => quill.format('italic', true)}><i className="fa fa-italic"></i></button>
+      <button id="link-button" onClick={() => quill.format('link', prompt('Enter link URL'))}><i className="fa fa-link"></i></button>
+      <button id="blockquote-button" onClick={() => quill.format('blockquote', true)}><i className="fa fa-quote-right"></i></button>
+      <button id="header-1-button" onClick={() => quill.format('header', 1)}><i className="fa fa-header"><sub>1</sub></i></button>
+      <button id="header-2-button" onClick={() => quill.format('header', 2)}><i className="fa fa-header"><sub>2</sub></i></button>
     </div>
   )
 }
